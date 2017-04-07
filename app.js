9652f36683da13a08778dd7518330f81474d7100
Matthew Parker <cadburylion@outlook.com>
2017-04-07 14:39:11 -0700

    copied in lab HTML and copied information for first question into my javascript file

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+'use strict';
+/////////////////////////////////////
+/* Problem 1 (this is your demo that we'll solve in class)
+Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
+"The sum of 4 and 7 is 11."
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
+
+// Write your code here
+function sum(a,b){ //eslint-disable-line
+
+}
+
+// Here is the test for sum(); uncomment it to run it
+// testSum(4, 7);
+
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+
+
+
+// // Asks the user to guess a random number, alerting them if they guess too high or too low
+//
+// var userName = prompt('What\'s your name?');
+// var myRandomNumber = Math.floor((Math.random() * 10) + 1);
+// var guessCorrect = false;
+// var guessAttempt = 3;
+//
+// var userGuess = prompt('Okay ' + userName + '. I\'m thinking of a random number between 1 and 10. What is it?');
+//   for(guessAttempt )
