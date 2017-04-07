6edabe2ea1bc85c224d51760c8400959cf4ba867
Matthew Parker <cadburylion@outlook.com>
2017-04-07 15:00:03 -0700

    started over using the prewritten starter code

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 'use strict';
-
-'use strict';
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
@@ -17,14 +15,73 @@ function sum(a,b){ //eslint-disable-line
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
+/////////////////////////////////////
+/* Problem 2
+Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+"The product of 5 and 9 is 45."
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
+// Write your code here
+function multiply(a,b){ //eslint-disable-line
 
-// // Asks the user to guess a random number, alerting them if they guess too high or too low
-//
-// var userName = prompt('What\'s your name?');
-// var myRandomNumber = Math.floor((Math.random() * 10) + 1);
-// var guessCorrect = false;
-// var guessAttempt = 3;
-//
-// var userGuess = prompt('Okay ' + userName + '. I\'m thinking of a random number between 1 and 10. What is it?');
-//   for(guessAttempt )
+}
+
+// Here is the test for multiply(); uncomment it to run it
+// testMultiply(5,9);
+
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+
+/////////////////////////////////////
+/* Problem 3
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Third element: "4 and 7 and 5 sum to 16."
+Fourth element: "The product of 4 and 7 and 5 is 140."
+IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
+
+// Write your code here
+function sumAndMultiply(a,b,c){ //eslint-disable-line
+
+}
+
+// Here is the test for sumAndMultiply(); uncomment it to run it
+// testSumAndMultiply(4,7,5);
+
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+
+/////////////////////////////////////
+/* Problem 4
+Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+"2,3,4 was passed in as an array of numbers, and 9 is their sum."
+IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
+
+// Write your code here
+var testArray = [2,3,4]; //eslint-disable-line
+
+function sumArray(testArray){ //eslint-disable-line
+
+}
+
+// Here is the test for sumArray(); uncomment it to run it
+
+// testSumArray(testArray);
+
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+
+/////////////////////////////////////
+/* Problem 5
+Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+"The numbers 2,3,4 have a product of 24."
+IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
+
+// Write your code here
+function multiplyArray(testArray){ //eslint-disable-line
+
+}
+
+// Here is the test for multiplyArray(); uncomment it to run it
+// testMultiplyArray(2,3,4);
+
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
